e85136c414ff327fcda08bfea94ab069cd98f288
hrry6 <harrysiloam06@gmail.com>
2024-01-31 19:53:31 +0700

    Template

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Gallery from "./components/Gallery";
+import Main from "./components/Main";
+import Mentors from "./components/Mentors";
+
+function App() {
+  return (
+    <div className="App">
+      <Main/>
+      <About/>
+      <Gallery/>
+      <Mentors/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
